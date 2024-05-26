25e0473d03a045d669c334e1fbf066acf08fde46
Dylan Szczepanski <adorevi9@gmail.com>
2024-05-26 17:59:58 -0500

    "PROGRESS HAHA"

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
     "fmt"
 	"net/http"
 	"strings"
+    "github.com/TheZoraiz/ascii-image-converter/aic_package"
+
 	"encoding/json"
 )
 
@@ -19,8 +21,8 @@ type Pokemon struct {
 }
 
 func Reader() string {
-    reader := bufio.NewReader(os.Stdin)   
     fmt.Println("What Pokémon info do you need?:")
+    reader := bufio.NewReader(os.Stdin)   
 
     key, err := reader.ReadString('\n')
     if err != nil {
@@ -33,7 +35,7 @@ func Reader() string {
 }
 
 func getPokemonData() (Pokemon, error) {
-    pokemon := "pikachu"
+    pokemon := Reader()
     url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", pokemon)
 	
     response, err := http.Get(url)
@@ -50,14 +52,37 @@ func getPokemonData() (Pokemon, error) {
 	return result, nil
 }
 
+func terminalOutput() {
+    data, err := getPokemonData()
+    if( err != nil) {
+        fmt.Print("idk something important")
+    }
+    // If file is in current directory. This can also be a URL to an image or gif.
+	filePath := data.Sprites.FrontDefault
+
+	flags := aic_package.DefaultFlags()
+
+	// This part is optional.
+	// You can directly pass default flags variable to aic_package.Convert() if you wish.
+	// There are more flags, but these are the ones shown for demonstration
+	flags.Dimensions = []int{50, 25}
+	flags.Colored = true
+	//flags.SaveTxtPath = "."
+	//flags.SaveImagePath = "."
+	flags.CustomMap = " .-=+#@"
+	//flags.FontFilePath = "./RobotoMono-Regular.ttf" // If file is in current directory
+	flags.SaveBackgroundColor = [4]int{50, 50, 50, 100}
+
+	asciiArt, err := aic_package.Convert(filePath, flags)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%v\n", asciiArt)
+
+}
+
 
 func main() {
-    pokemonData, err := getPokemonData()
-    if err != nil {
-        fmt.Printf("Error fetching Pokémon data: %v\n", err)
-        return
-    }
-
-    fmt.Println("name and id")
-    fmt.Println(pokemonData)
+    terminalOutput()
 }
